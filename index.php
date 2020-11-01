ad74064ca26ea20cc40cbdc2af55e313fec032bb
BrunoLagasse <brlagasse@laposte.net>
2020-11-01 16:55:45 +0100

    modification

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use App\Car;
+
+    $car = new Car(1);
+
+    try{
+        echo $car->start();
+    }
+    catch (Exception $e){
+        $car->setParkBrake(0);
+        echo 'Message: ' . $e->getMessage() . '<br>';
+    }
+    finally {
+        echo 'Ma voiture roule comme un donut.';
+    }
